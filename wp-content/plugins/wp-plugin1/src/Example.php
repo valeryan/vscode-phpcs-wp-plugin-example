2bea092f67844d244ffaac8043fa46955cae2909
Samuel Hilson <samrhilson@gmail.com>
2020-09-02 11:34:03 -0500

    Add example of wp plugins without multiple vendor folders

---
+++ wp-content/plugins/wp-plugin1/src/Example.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin 1 Example
+ *
+ * @package Plugin1
+ * @author  Samuel Hilson <samrhilson@gmail.com>
+ * @license MIT
+ */
+
+namespace Plugin1;
+
+class Example {
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		//
+	}
+}
---
+++ wp-content/plugins/wp-plugin2/src/class-example.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin 2 Example
+ *
+ * @package Plugin2
+ * @author  Samuel Hilson <samrhilson@gmail.com>
+ * @license MIT
+ */
+
+namespace Plugin2;
+
+class Example {
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		//
+	}
+}
